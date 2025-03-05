1efccdf55dd4dfafbedc25aebb38874c5024218e
Ramos223 <luisenriqueramos223@gmail.com>
2025-03-05 13:32:09 -0600

    priceTag added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,61 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:luis_application_2c/utils/my_tab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-  @override
+ @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Widget>  myTabs = [
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    const MyTab(iconPath: 'lib/icons/burger.png'),
+    const MyTab(iconPath: 'lib/icons/smoothie.png'),
+    const MyTab(iconPath: 'lib/icons/pancakes.png'),
+    const MyTab(iconPath: 'lib/icons/pizza.png')
+    ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        //Icono Izquierdo
-        leading: Icon(
-        Icons.menu,
-        color: Colors.grey[800]
-        ),
-        //Icono derecho
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 24.0),
-            child: Icon(Icons.person),
-          )
-        ],
-      ),
-      body: const Column(
-      children: [
-        //Texto principal
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 36, vertical: 18),
-          child: Row(
-            children: [
-              Text(
-                "Ï want to ",
-                style: TextStyle(fontSize: 32),
-                ),
-              Text("Eat",
-              style: TextStyle(
-                //Tamaño de letra
-                fontSize: 32,
-                //Negritas
-                fontWeight: FontWeight.bold,
-                //Subrayado
-                decoration: TextDecoration.underline
-              ),)
-            ],
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar( 
+          backgroundColor: Colors.transparent,
+          //Icono Izquierdo
+          leading: Icon(
+          Icons.menu,
+          color: Colors.grey[800]
           ),
+          //Icono Derecho
+          actions:const [
+            Padding(
+              padding: EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
         ),
-
-        //Pestañas (TabBar)
-
-        //Contenido de pestañas (TabBarView)
-
-        //Carrito (Card)
-      ]
-    )); 
+        body: Column(
+          children: [
+        //Texto principal
+         const Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+            child: Row(
+              children: [
+                Text("I want to  ", 
+                style: TextStyle(
+                  //Tipografia 
+                  fontFamily: "ComicStrip" ,
+                  //Tamaño de letra
+                  fontSize: 34
+                )
+                ),
+                Text("Eat", 
+                style: TextStyle(
+                  //Tipografia 
+                  fontFamily: "ComicStrip" ,
+                  //Tamaño de letra
+                  fontSize: 32,
+                  //Negritas
+                  fontWeight: FontWeight.bold,
+                  //Subrayado
+                  decoration: TextDecoration.underline
+                ),)
+              ],
+            ),
+          ),
+      
+      
+        //Pestaña (TaBar)TabBar(tabs: myTabs),
+        TabBar(tabs: myTabs),
+        Expanded(child: TabBarView(children: [DonutTab(), BurguerTab(), SmoothieTab(), PancakesTab(), PizzaTab(),])),
+        //Contenido(Cart)
+      
+          ],)
+      ),
+    );
   }
 }
