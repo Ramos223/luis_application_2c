4f59ccadaacef2acecd7766c018ed36c5b60b5ec
Ramos223 <luisenriqueramos223@gmail.com>
2025-03-19 13:38:14 -0600

    Adding new products

--- lib/tabs/burger_tab.dart
+++ lib/tabs/burger_tab.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:luis_application_2c/utils/donut_tile.dart';
 
 class BurgerTab extends StatelessWidget {
-  const BurgerTab({super.key});
-
+  // Lista de donas
+  final List donutsOnSale = [
+    // [donutFlavor, donutStore, donutPrice, donutColor, imageName]
+    ["Ice Cream","Krispy Kreme", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "Dunkin Donuts", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "Costco", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "Walmart", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+    ["Ice Cream","Krispy Kreme", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "Dunkin Donuts", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "Costco", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "Walmart", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+  ];
+ 
+  BurgerTab({super.key});
+ 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("BurgerTab")
-    );
+    //Cuadrícula
+    return GridView.builder(
+      //Cuántos elementos hay en nuestra lista
+        itemCount: donutsOnSale.length,
+        padding: const EdgeInsets.all(8.0),
+        //Organiza los elementos en una cuadricula
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          // Número de columnas
+          crossAxisCount: 2,
+          //Relación de aspecto (proporción)
+          childAspectRatio: 1/1.6),
+        itemBuilder: (context, index) {
+          return DonutTile(
+            donutFlavor: donutsOnSale[index][0],
+            donutStore: donutsOnSale[index][1],
+            donutPrice: donutsOnSale[index][2],
+            donutColor: donutsOnSale[index][3],
+            imageName: donutsOnSale[index][4],
+           
+          );
+        },
+      );
   }
 }
+ 
