dbe5deacfd39e05d422945980182c8378ba60559
Ramos223 <luisenriqueramos223@gmail.com>
2025-03-19 13:21:22 -0600

    UI finished

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -78,6 +78,58 @@ class _HomePageState extends State<HomePage> {
  
  
         //Carrito
+        Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(16),
+          child: Row(
+            //Esto alinia los elementos a los extremos
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  //Alinear horizontalmente una colmna
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("2 Items | \$45",
+                    style: TextStyle(
+                      fontSize: 18,fontWeight: FontWeight.bold),),
+                    Text('Delivery Charges included')
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pink,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 12
+                  )
+                  
+                ), 
+              child:const Row(
+                children:[
+                Icon(
+                  Icons.shopping_cart,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                
+                  Text(
+                    'View cart', 
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                      ),
+                ],
+              ))
+              ],
+          ),
+
+        )
       ];
   }
 }
